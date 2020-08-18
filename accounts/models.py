af70dc36cf2b2fb386ff56652e62fa36ee6f54aa
Vanessa Yoon <guswjd6005@gmail.com>
2020-08-18 21:16:26 +0900

    login add

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField('닉네임', max_length=20, null = True)
+
+    location = models.CharField('장소', max_length=50, null = True)
+    age = models.PositiveIntegerField('나이', default=1, null = True)
+    
+    def __str__(self):
+        return str
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login
+from .forms import UserCreationForm, ProfileForm
+
 
 # Create your views here.
 
@@ -10,15 +12,20 @@ def signup(request):
     if request.method=="POST":
         #회원가입 프로세스
         save_form = UserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        
         if save_form.is_valid():
             save_form.save()
-            
+            profile_form.save()
             ##회원가입후에 자동 로그인이 되는 프로세스
             user = authenticate(username=save_form.cleaned_data['username'], 
                                 password =save_form.cleaned_data['password1'])
             
             login(request,user)
             
+            tmp_profile = profile_form.save(commit = False)
+            tmp_profile.save()
+
             return redirect('index')
         else:
             
@@ -27,6 +34,7 @@ def signup(request):
     else:
         user_form = UserCreationForm()
         context['user_form'] = user_form
+        context['profile_form'] = ProfileForm()
         
         
         return render(request,'registration/signup.html', context)
