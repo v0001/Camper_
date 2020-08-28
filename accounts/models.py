815af49632c969bc0fa826e19068c49e9731a6c8
hyeyeonjung <hyeyeonijung@gmail.com>
2020-08-29 00:38:37 +0900

    login page

--- accounts/models.py
+++ accounts/models.py
@@ -15,8 +15,8 @@ class mypage(models.Model):
     # sex = models.BooleanField(choices=select, default=1, blank=False)
     # nickname = models.CharField('닉네임', max_length=20, blank=True, null=True)
     #blacktrue->빈칸입력가능 nulltrue -> 데이터의공백 빈칸입력불가
-
-    location = models.CharField('장소', max_length=50, blank=True, null=True)
+    
+    location = models.CharField('관심지역', max_length=50, blank=True, null=True)
     age = models.PositiveIntegerField('나이', default=1, blank=True, null=True)
 
     def __str__(self):
